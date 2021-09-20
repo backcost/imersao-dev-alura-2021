8821edfaf2f96ad8299c99290110019dd96b45c8
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-20 17:17:39 -0300

    depois de 3 tentativas, dizer o resultado

--- aula03/script.js
+++ aula03/script.js
@@ -3,17 +3,23 @@ function chutar() {
     let chute = parseInt(document.getElementById('valor').value)
 
     if (chute === numeroSecreto) {
-        elementoResultado.innerHTML = "Você acertou"
+        i = 0
+        elementoResultado.innerHTML = "Você acertou!"
     } else if (chute > 10 || chute < 0) {
         elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10"
     } else {
+        i++
         if (chute > numeroSecreto) {
             elementoResultado.innerHTML = `Errou, o número é menor que ${chute}!`
         } else {
             elementoResultado.innerHTML = `Errou, o número é maior que ${chute}!`
-        }
-        
+        } 
+    }
+    if (i === 3) {
+        i = 0
+        elementoResultado.innerHTML = `Você não acertou! O número era o ${numeroSecreto}!`
     }
 }
 
+let i = 0
 let numeroSecreto = parseInt(Math.random() * 11)
