d7651e9bc1f1f88a637797380042ebc050e502b6
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-21 15:07:56 -0300

    criado um botão para remover filme da tela

--- aula05/script.js
+++ aula05/script.js
@@ -1,19 +1,30 @@
 const adicionarFilme = () => {
     let filmeFavorito = document.getElementById("filme").value
     if (filmeFavorito.endsWith(".jpg")) {
+        listaFilmes.push(filmeFavorito)
         listarFilmesNaTela(filmeFavorito)
     } else {
         console.error('Endereço de filme inválido')
     }
+    document.getElementById("filme").value = ""
+}
+
+const removerFilme = () => {
+    let filmeFavorito = document.getElementById("filme").value
+    let temOFilme = listaFilmes.indexOf(filmeFavorito)
+    if (temOFilme !== -1) {
+        listaFilmes.splice(temOFilme, 1)
+        listarFilmesNaTela(filmeFavorito)
+    } else {
+        console.error('Filme não encontrado')
+    }
     document.getElementById('filme').value = ''
-    console.log(listaFilmes)
 }
 
 const listarFilmesNaTela = (filme) => {
     let elementoListaFilmes = document.getElementById('listaFilmes')
     elementoListaFilmes.innerHTML = ''
-    listaFilmes.push(filme)
-    
+
     for (let el of listaFilmes) {
         elementoListaFilmes.innerHTML += `<img src = ${el}>`
     }
