ccce4d892aaf4fa546fde8e3b3231a7b38289482
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-21 11:32:20 -0300

    guardar filmes em um array

--- aula05/script.js
+++ aula05/script.js
@@ -1,15 +1,22 @@
-function adicionarFilme() {
-    var filmeFavorito = document.getElementById("filme").value
+const adicionarFilme = () => {
+    let filmeFavorito = document.getElementById("filme").value
     if (filmeFavorito.endsWith(".jpg")) {
         listarFilmesNaTela(filmeFavorito)
     } else {
         console.error('Endereço de filme inválido')
     }
     document.getElementById('filme').value = ''
+    console.log(listaFilmes)
 }
 
-function listarFilmesNaTela(filme) {
-    var elementoFilmeFavorito = "<img src=" + filme + ">"
-    var elementoListaFilmes = document.getElementById('listaFilmes')
-    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+const listarFilmesNaTela = (filme) => {
+    let elementoListaFilmes = document.getElementById('listaFilmes')
+    elementoListaFilmes.innerHTML = ''
+    listaFilmes.push(filme)
+    
+    for (let el of listaFilmes) {
+        elementoListaFilmes.innerHTML += `<img src = ${el}>`
+    }
 }
+
+let listaFilmes = []
