8336b69c5d731df0952b9b165ff8053d9054151e
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-21 10:51:16 -0300

    código base

--- aula05/script.js
+++ aula05/script.js
@@ -1,10 +1,15 @@
-var listaFilmes = [
-    "https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
-    "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
-    "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/90/98/20169244.jpg"
-  ];
-  
-  for (var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src=" + listaFilmes[i] + ">");
-  }
-  
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value
+    if (filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito)
+    } else {
+        console.error('Endereço de filme inválido')
+    }
+    document.getElementById('filme').value = ''
+}
+
+function listarFilmesNaTela(filme) {
+    var elementoFilmeFavorito = "<img src=" + filme + ">"
+    var elementoListaFilmes = document.getElementById('listaFilmes')
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+}
