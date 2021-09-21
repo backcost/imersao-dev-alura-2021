0fbaf7b016d6bdf766673e428f07cd01fb6ad10b
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-21 16:02:57 -0300

    outro input para nome do filme + reformulando funcionamento da lista

--- aula05/script.js
+++ aula05/script.js
@@ -1,33 +1,46 @@
 const adicionarFilme = () => {
-    let filmeFavorito = document.getElementById("filme").value
-    if (filmeFavorito.endsWith(".jpg")) {
-        listaFilmes.push(filmeFavorito)
-        listarFilmesNaTela(filmeFavorito)
+    let urlFilme = document.getElementById("filme").value
+    let nomeFilme = document.getElementById("filme__nome").value
+
+    if (urlFilme.endsWith(".jpg")) {
+        listaFilmes.push([nomeFilme, urlFilme])
+        listarFilmesNaTela(urlFilme, nomeFilme)
     } else {
         console.error('Endereço de filme inválido')
     }
     document.getElementById("filme").value = ""
+    document.getElementById("filme__nome").value = ""
 }
 
 const removerFilme = () => {
-    let filmeFavorito = document.getElementById("filme").value
-    let temOFilme = listaFilmes.indexOf(filmeFavorito)
+    let urlFilme = document.getElementById("filme").value
+    let nomeFilme = document.getElementById("filme__nome").value
+
+    let temOFilme = -1
+    for (let i = 0; i < listaFilmes.length; i++) {
+        if (listaFilmes[i][1] === urlFilme || listaFilmes[i][0] === nomeFilme) {
+            temOFilme = i
+            break
+        }
+    }
+
     if (temOFilme !== -1) {
         listaFilmes.splice(temOFilme, 1)
-        listarFilmesNaTela(filmeFavorito)
+        listarFilmesNaTela(urlFilme)
     } else {
         console.error('Filme não encontrado')
     }
     document.getElementById('filme').value = ''
 }
 
-const listarFilmesNaTela = (filme) => {
+const listarFilmesNaTela = (filme, titulo) => {
     let elementoListaFilmes = document.getElementById('listaFilmes')
     elementoListaFilmes.innerHTML = ''
 
     for (let el of listaFilmes) {
-        elementoListaFilmes.innerHTML += `<img src = ${el}>`
+        elementoListaFilmes.innerHTML += `<img src = ${el[1]}>`
     }
+    console.log(listaFilmes)
 }
 
 let listaFilmes = []
