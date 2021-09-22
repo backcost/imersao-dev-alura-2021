e0ece497cea29eea301285b88a372462d44b7640
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-22 13:25:08 -0300

    organizando código para o meu estilo

--- aula06/script.js
+++ aula06/script.js
@@ -1,58 +1,53 @@
-//          chave.  valor.
-var rafa = { nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 };
-var paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
-var gui = { nome: "Gui", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 };
-
 function calculaPontos(jogador) {
-  var pontos = jogador.vitorias * 3 + jogador.empates;
-  return pontos;
+    return jogador.vitorias * 3 + jogador.empates
 }
-
-rafa.pontos = calculaPontos(rafa);
-paulo.pontos = calculaPontos(paulo);
-gui.pontos = calculaPontos(gui);
-
-var jogadores = [rafa, paulo, gui];
-
-function exibeJogadoresNaTela(jogadores) {
-  var elemento = "";
-  for (var i = 0; i < jogadores.length; i++) {
-    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
-    elemento += "<td>" + jogadores[i].vitorias + "</td>";
-    elemento += "<td>" + jogadores[i].empates + "</td>";
-    elemento += "<td>" + jogadores[i].derrotas + "</td>";
-    elemento += "<td>" + jogadores[i].pontos + "</td>";
-    elemento +=
-      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
-    elemento +=
-      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
-    elemento +=
-      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
-    elemento += "</tr>";
-  }
-
-  var tabelaJogadores = document.getElementById("tabelaJogadores");
-  tabelaJogadores.innerHTML = elemento;
-}
-
-exibeJogadoresNaTela(jogadores);
 
 function adicionarVitoria(i) {
-  var jogador = jogadores[i];
-  jogador.vitorias++;
-  jogador.pontos = calculaPontos(jogador);
-  exibeJogadoresNaTela(jogadores);
+    let jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadoresNaTela(jogadores)
 }
-
+  
 function adicionarEmpate(i) {
-  var jogador = jogadores[i];
-  jogador.empates++;
-  jogador.pontos = calculaPontos(jogador);
-  exibeJogadoresNaTela(jogadores);
+    let jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadoresNaTela(jogadores)
+}
+  
+function adicionarDerrota(i) {
+    let jogador = jogadores[i]
+    jogador.derrotas++
+    exibeJogadoresNaTela(jogadores)
 }
 
-function adicionarDerrota(i) {
-  var jogador = jogadores[i];
-  jogador.derrotas++;
-  exibeJogadoresNaTela(jogadores);
+function exibeJogadoresNaTela(jogadores) {
+    let elemento = ""
+    for (let i = 0; i < jogadores.length; i++) {
+        elemento += 
+        `<tr><td>${jogadores[i].nome}</td>
+        <td>${jogadores[i].vitorias}</td>
+        <td>${jogadores[i].empates}</td>
+        <td>${jogadores[i].derrotas}</td>
+        <td>${jogadores[i].pontos}</td>
+        <td><button onClick="adicionarVitoria(${i})">Vitória</button></td>
+        <td><button onClick="adicionarEmpate(${i})">Empate</button></td>
+        <td><button onClick="adicionarDerrota(${i})">Derrota</button></td></tr>`
+    }
+    let tabelaJogadores = document.getElementById("tabelaJogadores")
+    tabelaJogadores.innerHTML = elemento
 }
+
+
+let rafa = { nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1, pontos: 0 }
+let paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 }
+let gui = { nome: "Gui", vitorias: 1, empates: 1, derrotas: 2, pontos: 0 }
+
+let jogadores = [rafa, paulo, gui]
+
+rafa.pontos = calculaPontos(rafa)
+paulo.pontos = calculaPontos(paulo)
+gui.pontos = calculaPontos(gui)
+
+exibeJogadoresNaTela(jogadores)
